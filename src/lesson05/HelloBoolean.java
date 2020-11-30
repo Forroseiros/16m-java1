001c1bd565f3d45880d6fbbce0c4a910b9489e37
andron13 <andron13@gmail.com>
2020-11-30 12:05:32 +0100

    boolean intro

--- src/lesson05/HelloBoolean.java
+++ src/lesson05/HelloBoolean.java
@@ -3,15 +3,41 @@ package lesson05;
 public class HelloBoolean {
     public static void main(String[] args) {
         // reviewInt();
+        //booleanIntro();
+        //magicGame();
+        magicGame2();
+    }
+
+    public static void magicGame2() {
+        boolean a = (7 + 8) * 5 > 7 + 8 * 5;
+        boolean b = (7 + 8) * 4 != 7 + 4 * 5;
+        boolean c = 3 + 4 > 9 + 1 & 16 - 5 > 3 * 4;
+        boolean d = 16 / 2 < 6 + 2 | 4 + 5 <= 4 * 5;
+        boolean e = !(3 * 4 < 7 + 8);
+        System.out.println(a + "\n" + b + "\n" + c + "\n" + d + "\n" + e);
+    }
+
+    public static void magicGame() {
+        int a = 5;
+        int b = 10;
+        boolean c = a > b;
+        // boolean d = 10;
+        System.out.println(c);
+        System.out.println(a <= b);
+        System.out.println(a >= b);
+        System.out.println(true);
+    }
+
+    public static void booleanIntro() {
         boolean a = true;
         boolean b = false;
         // boolean c = 1;
-        boolean c = a && b;
+        boolean c = a && b; // И and
         System.out.println(c);
-        System.out.println(a || b);
-        System.out.println(!a);
+        System.out.println(a || b); // ИЛИ OR
+        System.out.println(!a); // отрицание НЕ , NOT
         boolean d = !a;
-        boolean f = a ^ b;
+        boolean f = a ^ b; // XOR
         System.out.println(f);
     }
 
