cfb0ad8a574f7b811347b589526c112a3ac6c906
andron13 <andron13@gmail.com>
2020-12-09 12:59:40 +0100

    color

---
+++ src/homework12/TestHomeWork.java
@@ -0,0 +1,13 @@
+package homework12;
+
+import lesson12.MyColor;
+
+public class TestHomeWork {
+    public static void main(String[] args) {
+        System.out.println(MyColor.ANSI_BLUE + "text" + MyColor.ANSI_RESET);
+        System.out.println(MyColor.ANSI_RED
+                + MyColor.ANSI_BLACK_BACKGROUND
+                + "sfsdf");
+
+    }
+}
