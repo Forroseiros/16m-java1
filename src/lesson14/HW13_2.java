74131f96b7028d83864c85ef1f2fec3974bf1c4b
andron13 <andron13@gmail.com>
2020-12-11 12:37:10 +0100

    двери и окна

---
+++ src/lesson14/HW13_2.java
@@ -0,0 +1,25 @@
+package lesson14;
+
+public class HW13_2 {
+    public static void main(String[] args) {
+        System.out.println(lastChars("last", "chars")); //→"ls"
+        System.out.println(lastChars("yo", "java")); //→"ya"
+        System.out.println(lastChars("hi", "")); //→"h@"
+        System.out.println(lastChars("", "")); //→"h@"
+    }
+
+    private static String lastChars(String a, String b) {
+        String x = a;
+        String y = b;
+        if (x.length() == 0) {
+            x = "@";
+        }
+
+        if (y.length() == 0) {
+            y = "@";
+        }
+        String firstLetterFirstWord = x.substring(0, 1);
+        String lastLetterLastWord = y.substring(y.length() - 1);
+        return firstLetterFirstWord + lastLetterLastWord;
+    }
+}
---
+++ src/lesson14/HW13_4.java
@@ -0,0 +1,40 @@
+package lesson14;
+
+public class HW13_4 {
+    // static double framePrice = 100;
+    public static void main(String[] args) {
+        double finalPrise = countCost(8, 4, 180, 175);
+        System.out.println("Итого " + finalPrise);
+
+    }
+
+    private static double countCost(int doors,
+                                    int windows,
+                                    double high,
+                                    double wide) {
+        return doorsPrice(doors) + windowsPrise(windows, high, wide);
+    }
+
+    private static double windowsPrise(int windows,
+                                       double high,
+                                       double wide) {
+        double meterHigh = high / 100;
+        double meterWide = wide / 100;
+        double framePrice = 100;
+        double glassPrice = 100;
+        double perimeter = (meterWide + meterHigh) * 2;
+        double area = meterWide * meterHigh;
+        return (perimeter * framePrice + area * glassPrice) * windows;
+    }
+
+    private static double doorsPrice(int doors) {
+        double doorsMeterPrice = 120.0;
+        double a = 2.0;
+        double b = 0.9;
+        double perimeter = (a + b) * 2;
+        double area = a * b;
+        double framePrice = 100;
+
+        return (perimeter * framePrice + area * doorsMeterPrice) * doors;
+    }
+}
