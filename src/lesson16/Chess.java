1c12e24da0b8d42ba46c8d92ab9ea57f8b74cc2c
andron13 <andron13@gmail.com>
2020-12-15 13:05:08 +0100

    l-15

--- src/lesson16/Chess.java
+++ src/lesson16/Chess.java
@@ -1,30 +1,31 @@
-package lesson15;
+package lesson16;
 
 public class Chess {
     public static void main(String[] args) {
-        //chess(8);
-        multiplyTable(10);
-    }
-
-    private static void multiplyTable(int lines) {
-        // 1. Таблица
-        // 2. содержимое, произвидение
-        // 3. Отформатировать
-        for (int i = 1; i <=lines; i++) {
-            for (int j = 1; j <= lines; j++) {
-                System.out.print(i * j + " ");
-            }
-            System.out.println();
-        }
+        chess(8);
     }
 
     private static void chess(int line) {
         for (int i = 0; i < line; i++) {
-            for (int j = 0; j < line / 2; j++) {
-                System.out.print("*-");
+            for (int j = 0; j < line; j++) {
+                String place = formatChess(i, j);
+                System.out.print(place);
             }
             System.out.println();
         }
     }
 
+    private static String formatChess(int i, int j) {
+        String result = "";
+        String star = "*";
+        String minus = "-";
+        if (i % 2 != 0 && j % 2 != 0) {
+            result = star;
+        } else if (i % 2 == 0 && j % 2 == 0) {
+            result = star;
+        } else {
+            result = minus;
+        }
+        return result;
+    }
 }
