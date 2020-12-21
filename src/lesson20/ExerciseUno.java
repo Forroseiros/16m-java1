73cc41af7c10725ee3411afda93586b939c9d362
andron13 <andron13@gmail.com>
2020-12-21 12:37:10 +0100

    MagicMain - L 20

--- src/lesson20/ExerciseUno.java
+++ src/lesson20/ExerciseUno.java
@@ -17,16 +17,20 @@ public class ExerciseUno {
         int[] c1 = new int[]{4, 5};
         int[] c2 = new int[]{4, 2};
         int[] c3 = new int[]{3, 5};
-
+        // Given an int array length 2, return true if it does not contain a 2 or 3.
         System.out.println(no23(c1)); //→ true
         System.out.println(no23(c2)); //→ false
         System.out.println(no23(c3)); //→ false
     }
 
     private static boolean no23(int[] array) {
+        // private static boolean is23(int[] array)
+        // return array[0] != 2 && array[0] != 3 && array[1] != 2 && array[1] != 3;
+        if (array[0] == 2 || array[0] == 3 || array[1] == 2 || array[1] == 3) {
+            return false;
+        }
 
-
-        return false;
+        return true;
     }
 
     private static int[] middleWay(int[] a, int[] b) {
